f980bb21caed78dcc3af653bac1e8ff03c4d5480
diaaaaana <didaromaalan2005@gmail.com>
2024-12-05 21:57:06 -0500

    creacion de proywecto

---
+++ src/main/java/edu/i202225123/cl2_romero_diana/Cl2RomeroDianaApplication.java
@@ -0,0 +1,13 @@
+package edu.i202225123.cl2_romero_diana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cl2RomeroDianaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cl2RomeroDianaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/i202225123/cl2_romero_diana/Cl2RomeroDianaApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.i202225123.cl2_romero_diana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cl2RomeroDianaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
